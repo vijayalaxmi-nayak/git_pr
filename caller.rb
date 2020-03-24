c33aefc753cea8ca7a83d4a6847c0fdf607cf5ca
vijayalaxmi-nayak <vijayalaxmi@amagi.com>
2020-03-24 13:00:48 +0530

    added rest-api

--- caller.rb
+++ caller.rb
@@ -8,7 +8,17 @@ require 'rest-client'
       # h = First.new()
       # h.hello3((JSON.parse(event['body']))['id'])
       # h.parse((JSON.parse(event['body']))['id'])
-      RestClient.post "http://localhost:3000/messages", {'title' => (JSON.parse(event['body']))['id'] }.to_json, {content_type: :json, accept: :json}
-    end  
+      RestClient.post "http://18.224.69.99/api/v1/sessions/zUytxsjVLL3-zuLc2iea/medias", 
+      {'media_type' => 'video', "account_id" => "1",
+  "asset_id" => (JSON.parse(event['body']))['id'] }.to_json, {content_type: :json, accept: :json}  
+      { 
+        statusCode: 200, 
+        body: "successful upload!"
+      }
+    rescue StandardError => e  
+      puts e.message  
+      puts e.backtrace.inspect  
+      { statusCode: 400, body: JSON.generate("Invalid!") }
+    end
   end
 
